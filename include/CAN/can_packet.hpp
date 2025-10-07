7c86dcb1f61a2866eb904944b4a1a6bd3e1c6b4f
zen4215 <44k1.373@gmail.com>
2025-10-07 15:02:21 +0900

    add: define can_packet and manager interface

--- include/CAN/can_packet.hpp
+++ include/CAN/can_packet.hpp
@@ -1,4 +1,14 @@
 #ifndef CAN_PACKET_SEEN
 #define CAN_PACKET_SEEN
 
+#include <array>
+
+struct CANPacket {
+    CANPacket(int set_id);
+    void Clear();
+
+    int ID;
+    std::array<uint8_t, 8> buf;
+};
+
 #endif
