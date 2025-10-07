63d2ca6d8b098b71f270855b07d0788b2abde0f9
zen4215 <44k1.373@gmail.com>
2025-10-07 16:24:31 +0900

    add: define omni_wheel and wheels interface

--- include/Omni/omni_wheels.hpp
+++ include/Omni/omni_wheels.hpp
@@ -1,4 +1,16 @@
 #ifndef OMNI_WHEELS_SEEN
 #define OMNI_WHEELS_SEEN
 
+#include "omni_wheel.hpp"
+
+template<int num>
+class OmniWheels {
+    public:
+        OmniWheels(double first_omni_angle);
+        Run(double vecotor_x, double vector_y, double rotation);
+    private:
+        std::array<OmniWheel, num> Wheels;
+        double m_input_count;
+};
+
 #endif
