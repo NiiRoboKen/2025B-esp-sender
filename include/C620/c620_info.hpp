02a0b9b5abdf5c7371d2015bfbc9642615db7ee1
zen4215 <44k1.373@gmail.com>
2025-10-07 15:51:02 +0900

    add: define c620 and c620_info interface

--- include/C620/c620_info.hpp
+++ include/C620/c620_info.hpp
@@ -1,4 +1,19 @@
 #ifndef C620_INFO_SEEN
 #define C620_INFO_SEEN
 
+#include "CAN/can_packet.hpp"
+
+struct C620Info {
+    public:
+        int MotorID;
+        int16_t Angle();
+        int16_t RPM();
+        int16_t Current();
+        uint8_t Temp();
+        bool ReceiveFeedback();
+    private:
+        CANPacket FeedbackPacket;
+};
+
+
 #endif
